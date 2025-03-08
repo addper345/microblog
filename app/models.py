3e37e4c5ba28c36bbeec56bde0a4d49350a2a3d6
Aden P <addper345@gmail.com>
2025-03-08 17:39:10 -0500

    Prepare User model for flask-login

--- app/models.py
+++ app/models.py
@@ -4,8 +4,10 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -20,6 +22,8 @@ class User(db.Model):
         
     def __repr__(self):
         return '<User {}>'.format(self.username)
+        
+    
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -28,3 +32,7 @@ class Post(db.Model):
         index=True, default=lambda: datetime.now(timezone.utc))
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
     author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+@login.user_loader
+    def load_user(id):
+        return db.session.get(User, int(id))
