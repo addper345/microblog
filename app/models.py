e41ace58222271797741df8674468ce133d18460
Aden P <addper345@gmail.com>
2025-03-10 11:12:24 -0400

    Setup profile avatars

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -22,6 +23,10 @@ class User(UserMixin, db.Model):
         
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
         
     
 
