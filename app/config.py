b217c705bcd881319a5a4856f71c55ab6d0b6c06
Aden P <addper345@gmail.com>
2025-03-22 01:03:16 -0400

    Make send_email function asynchronous

--- app/config.py
+++ app/config.py
@@ -1,5 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
@@ -9,5 +14,5 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['addper345@gmail.com']
+    ADMINS = ['MS_EzRxBZ@trial-zkq340ez26xgd796.mlsender.net']
     POSTS_PER_PAGE = 3
